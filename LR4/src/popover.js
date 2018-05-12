1920d55bf95b416610300a64aef9292d1abeb53c
AnatoliyShilov <anatolishil@gmail.com>
2018-05-12 19:33:47 +0300

    Add files via upload

---
+++ LR4/src/popover.js
@@ -0,0 +1,27 @@
+class Popover
+{
+    constructor(name, text, element)
+    {
+        this.name = name;
+        var content = 
+            '<div id = "' + this.name + '" class = "rama background popover">' + 
+                text + 
+            '</div>';
+        this.name = '#' + this.name;
+        $('body').append(content);
+        element = $(element);
+        $(this.name).hide().offset({top: element.offset().top, left: element.offset().left + element.outerWidth(true) + $(this.name).outerWidth(true)});
+    }
+
+    hidePopover(ms)
+    {
+        setTimeout('$("' + this.name + '").hide();', ms);
+    }
+
+    showPopover(ms)
+    {
+        setTimeout('$("' + this.name + '").show()', ms);
+    }
+}
+
+
--- LR4/src/test.js
+++ LR4/src/test.js
@@ -2,6 +2,18 @@ window.onload = function()
 {
     createMenu();
     startTime();
+    var q1 = new Popover('q1Pop', 'q1', 'input[name="q1"]');
+    for (var i = 0; i < document.testForm.q1.length; i++)
+    {
+        document.testForm.q1[i].onmouseover = function (){ q1.showPopover(10); };
+        document.testForm.q1[i].onmouseout = function (){ q1.hidePopover(10); };
+    }
+    var q2 = new Popover('q2Pop', 'q2', 'select[name="q2"]');
+    document.testForm.q2.onmouseover = function (){ q2.showPopover(10); };
+    document.testForm.q2.onmouseout = function (){ q2.hidePopover(10); };
+    var q3 = new Popover('q3Pop', 'q3', 'textarea[name="q3"]');
+    document.testForm.q3.onmouseover = function (){ q3.showPopover(10); };
+    document.testForm.q3.onmouseout = function (){ q3.hidePopover(10); };
 }
 
 function validateForm( )
